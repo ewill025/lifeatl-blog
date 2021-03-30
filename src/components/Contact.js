19a8a398f397232e5fcf9a06743bb2a43b7675ab
ewill025 <ericwilliamsjr@live.com>
2021-03-30 16:02:10 -0400

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,30 +3,37 @@ import React from "react";
 
 import emailjs from "emailjs-com";
 
-
 export default function Contact() {
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('service_pbimhvl', 'template_lv9koh4', e.target, 'user_kMd017HSsFPqP8sXPG4os')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_pbimhvl",
+        "template_lv9koh4",
+        e.target,
+        "user_kMd017HSsFPqP8sXPG4os"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-      e.target.reset()
+        }
+      );
+    e.target.reset();
   }
-  
-  
-    return (
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-col text-center w-full mb-12">
-          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-            Contact Us
-          </h1>
-        </div>
-        <form className="contact-form" onSubmit={sendEmail}>
-          <div className="lg:w-1/2 md:w-2/3 mx-auto">
+
+  return (
+    <div className="container px-5 py-24 mx-auto">
+      <div className="flex flex-col text-center w-full mb-12">
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+          Contact Us
+        </h1>
+      </div>
+      <form className="contact-form" onSubmit={sendEmail}>
+        <div className="lg:w-1/2 md:w-2/3 mx-auto">
           <div className="flex flex-wrap -m-2">
             <div className="p-2 w-1/2">
               <div className="relative">
@@ -70,15 +77,18 @@ export default function Contact() {
               </div>
             </div>
             <div className="p-2 w-full">
-              <button
-                className="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg"
-              >
+              <button className="flex mx-auto text-white bg-green-600 border-0 py-2 px-8 focus:outline-none hover:bg-green-700 rounded text-lg">
                 Submit
               </button>
             </div>
             <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
               <span className="inline-flex">
-                <a href="https://www.facebook.com/LifeATL229/" target="_blank" rel="noreferrer" className="text-green-700">
+                <a
+                  href="https://www.facebook.com/LifeATL229/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-green-700"
+                >
                   <svg
                     fill="currentColor"
                     stroke-linecap="round"
@@ -90,7 +100,12 @@ export default function Contact() {
                     <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                   </svg>
                 </a>
-                <a href="https://twitter.com/lifeatl_?lang=en" target="_blank" rel="noreferrer" className="ml-4 text-green-700">
+                <a
+                  href="https://twitter.com/lifeatl_?lang=en"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="ml-4 text-green-700"
+                >
                   <svg
                     fill="currentColor"
                     stroke-linecap="round"
@@ -102,7 +117,12 @@ export default function Contact() {
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                   </svg>
                 </a>
-                <a href="https://www.instagram.com/lifeatl/?hl=en"  target="_blank" rel="noreferrer" className="ml-4 text-green-700">
+                <a
+                  href="https://www.instagram.com/lifeatl/?hl=en"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="ml-4 text-green-700"
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -123,7 +143,12 @@ export default function Contact() {
                     <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                   </svg>
                 </a>
-                <a href="https://lifeatl.tumblr.com/"  target="_blank" rel="noreferrer" className="ml-4 text-green-700">
+                <a
+                  href="https://lifeatl.tumblr.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="ml-4 text-green-700"
+                >
                   <svg
                     fill="currentColor"
                     stroke-linecap="round"
@@ -139,8 +164,7 @@ export default function Contact() {
             </div>
           </div>
         </div>
-        </form>
-        
-      </div>
-    );
+      </form>
+    </div>
+  );
 }
