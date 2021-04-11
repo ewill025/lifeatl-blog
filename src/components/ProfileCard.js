42758b85f1046d3ca5a98a245d1e960c9b5131a1
ewill025 <ericwilliamsjr@live.com>
2021-04-11 19:02:35 -0400

    Update ProfileCard.js

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,13 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams} from "react-router-dom";
 import sanityClient from "../client.js";
-import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 
-const builder = imageUrlBuilder(sanityClient);
-function urlFor(source) {
-  return builder.image(source);
-}
 const serializers = {
   types: {
     code: (props) => (
